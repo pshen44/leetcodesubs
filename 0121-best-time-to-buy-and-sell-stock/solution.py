3f9d7ec2cfc592251051059c3c9a37789bd8db75
Patrick Shen <pzshen44@gmail.com>
2025-05-29 07:43:59 +0000

    [LeetCode Sync] Runtime - 113 ms (38.40%), Memory - 19.1 MB (36.83%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        left, right = 0, 1
+        max_profit = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                max_profit = max(max_profit, profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+
+
+        
