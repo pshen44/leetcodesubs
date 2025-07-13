eb0d274cdecbd5dc52798d11bf9313ffe7443408
Patrick Shen <pzshen44@gmail.com>
2025-07-13 05:24:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (100.00%)

---
+++ 0019-remove-nth-node-from-end-of-list/solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
+        p1 = dummy
+        p2 = head
+
+        while n > 0:
+            p2 = p2.next
+            n -= 1
+        while p2:
+            p2 = p2.next
+            p1 = p1.next
+
+        p1.next = p1.next.next
+        return dummy.next
+
+
+        
