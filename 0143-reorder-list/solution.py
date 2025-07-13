fc91844daf4941cd213861ff00a611e30dc8691c
Patrick Shen <pzshen44@gmail.com>
2025-07-13 04:07:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.4 MB (100.00%)

---
+++ 0143-reorder-list/solution.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        p1 = head
+        p2 = prev
+
+        while p2:
+            tmp1, tmp2 = p1.next, p2.next
+            p1.next = p2
+            p2.next = tmp1
+            p2 = tmp2
+            p1 = tmp1
+
+
