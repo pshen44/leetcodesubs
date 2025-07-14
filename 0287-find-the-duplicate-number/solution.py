05715971ccc2c35b0a6b755d0ec0e530a7c22fe0
Patrick Shen <pzshen44@gmail.com>
2025-07-14 17:34:17 +0000

    [LeetCode Sync] Runtime - 34 ms (27.83%), Memory - 29.1 MB (99.95%)

---
+++ 0287-find-the-duplicate-number/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        cur = 0
+        while True:
+            cur = nums[cur]
+            slow = nums[slow]
+            if cur == slow:
+                return cur
