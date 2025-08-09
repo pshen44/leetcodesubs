b60fde98d520f01c1b093c6787e0795d2faf3923
Patrick Shen <pzshen44@gmail.com>
2025-08-09 23:56:03 +0000

    [LeetCode Sync] Runtime - 135 ms (32.30%), Memory - 18.2 MB (100.00%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = 0
-        maxLen = 0
-        mostfreq = 0
-        strMap = defaultdict()
-        for r in range(len(s)):
-            strMap[s[r]] = 1 + strMap.get(s[r], 0)
-            mostfreq = max(mostfreq, strMap[s[r]])
-            while (r - l + 1) - mostfreq > k:
-            # streak broken
-                strMap[s[l]] -= 1
+        l, r = 0, 0
+        maxLength = 0
+        mostFreq = 0
+        strmap = defaultdict()
+        while r < len(s):
+            strmap[s[r]] = 1 + strmap.get(s[r], 0)
+            mostFreq = max(mostFreq, strmap[s[r]])
+            while (r - l + 1) - mostFreq > k:
+                strmap[s[l]] -= 1
                 l += 1
-            maxLen = max(r - l + 1, maxLen)
-        return maxLen
-
-
-
-
+            maxLength = max(maxLength, r - l + 1)
+            r += 1
+        return maxLength
+                
+        
