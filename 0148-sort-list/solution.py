de910bae2fb3b85c3d34a2bbe70f909b4fed9ea4
Patrick Shen <pzshen44@gmail.com>
2025-08-11 19:02:05 +0000

    [LeetCode Sync] Runtime - 188 ms (26.99%), Memory - 32.9 MB (100.00%)

---
+++ 0148-sort-list/solution.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        left = head
+        right = self.getmid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    def getmid(self, node):
+        slow, fast = node, node.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    def merge(self, l1, l2):
+        tail = dummy = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+            
+        return dummy.next
