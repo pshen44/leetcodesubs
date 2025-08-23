809838653cfc369173f89d6fa4998d367550e962
Patrick Shen <pzshen44@gmail.com>
2025-08-23 03:11:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- 0022-generate-parentheses/solution.py
+++ 0022-generate-parentheses/solution.py
@@ -1,23 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #if open < n add open
-        #if closed < open add closed
-        #all equal = done
-        stack = []
         res = []
-        def backtrack(closedN, openN):
-            if openN == closedN == n:
-                res.append("".join(stack))
-            if openN < n:
-                stack.append("(")
-                backtrack(closedN, openN + 1)
-                stack.pop()
-
-            if closedN < openN:
-                stack.append(")")
-                backtrack(closedN + 1, openN)
-                stack.pop()
-            
-        backtrack(0, 0)
+        def backtrack(opened, closed, path):
+            if opened < n:
+                backtrack(opened + 1, closed, path + '(')
+            if closed < opened:
+                backtrack(opened, closed + 1, path + ')')
+            if closed == n and opened == n:
+                res.append(path)
+                return
+        backtrack(0, 0, "")
         return res
-
