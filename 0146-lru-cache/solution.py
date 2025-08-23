89e0360ed6e428078e2651b9b7695323b557c4e2
Patrick Shen <pzshen44@gmail.com>
2025-08-23 04:15:30 +0000

    [LeetCode Sync] Runtime - 163 ms (10.32%), Memory - 77.9 MB (18.03%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -2,25 +2,33 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
         self.cache = {}
-        self.head = Node(0,0) #lru
-        self.tail = Node(0,0) #mru
-        self.head.next = self.tail
-        self.tail.prev = self.head
 
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
+        self.left = Node(-1, -1)
+        self.right = Node(-1, -1)
+        self.left.next = self.right
+        self.right.prev = self.left
+    
     def insert(self, node):
-        prev, nxt = self.tail.prev, self.tail
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        prv = self.right.prev
+        nxt = self.right
+        prv.next = node
+        nxt.prev = node
+        node.next = nxt
+        node.prev = prv
+    
+    def remove(self, node):
+        prv = node.prev
+        nxt = node.next
+        nxt.prev = prv
+        prv.next = nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -33,14 +41,15 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
+
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
-        if len(self.cache) > self.cap:
-            lru = self.head.next
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
 
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
