d218e4d624f909fe1162ff967eea9578b27b2a0d
Patrick Shen <pzshen44@gmail.com>
2025-08-29 01:49:11 +0000

    [LeetCode Sync] Runtime - 118 ms (23.22%), Memory - 35.4 MB (10.70%)

--- 0739-daily-temperatures/solution.py
+++ 0739-daily-temperatures/solution.py
@@ -2,9 +2,10 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = (i - stackInd)
-            stack.append([t, i])
+        for i, v in enumerate(temperatures):
+            while stack and v > stack[-1][0]:
+                tempVal, tempInd = stack.pop()
+                res[tempInd] = i - tempInd
+            stack.append([v,i])
         return res
+
