c0aa692a5ba4508c384078bfcac1b105d58fc592
Patrick Shen <pzshen44@gmail.com>
2025-09-14 02:00:27 +0000

    [LeetCode Sync] Runtime - 3 ms (89.92%), Memory - 21.3 MB (100.00%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        res = []
         heap = []
-        for num in count.keys():
-            heapq.heappush(heap, (count[num], num))
+        res = []
+        count = Counter(nums)
+        for n in count.keys():
+            heapq.heappush(heap, (count[n], n))
             if len(heap) > k:
                 heapq.heappop(heap)
 
         for i in range(k):
             res.append(heapq.heappop(heap)[1])
         return res
+
