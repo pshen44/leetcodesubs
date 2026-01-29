9494a37f94e7fb19008d669c8e6733344edcf569
Patrick Shen <pzshen44@gmail.com>
2026-01-29 19:23:30 +0000

    [LeetCode Sync] Runtime - 3 ms (54.75%), Memory - 20.8 MB (10.68%)

--- 0150-evaluate-reverse-polish-notation/solution.py
+++ 0150-evaluate-reverse-polish-notation/solution.py
@@ -1,31 +1,30 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for i in range(len(tokens)):
-            if tokens[i] == '+':
-                r = int(stack[-1]) + int(stack[-2])
+        for token in tokens:
+            if token == '+':
+                res = int(stack[-1]) + int(stack[-2])
                 stack.pop()
                 stack.pop()
-                stack.append(int(r))
+                stack.append(int(res))
                 continue
-            if tokens[i] == '-':
-                r = int(stack[-2]) - int(stack[-1])
+            if token == '*':
+                res = int(stack[-1]) * int(stack[-2])
                 stack.pop()
                 stack.pop()
-                stack.append(int(r))
+                stack.append(int(res))
                 continue
-            if tokens[i] == '*':
-                r = int(stack[-1]) * int(stack[-2])
+            if token == '/':
+                res = int(stack[-2]) / int(stack[-1])
                 stack.pop()
                 stack.pop()
-                stack.append(int(r))
+                stack.append(int(res))
                 continue
-            if tokens[i] == '/':
-                r = int(stack[-2]) / int(stack[-1])
+            if token == '-':
+                res = int(stack[-2]) - int(stack[-1])
                 stack.pop()
                 stack.pop()
-                stack.append(int(r))
+                stack.append(int(res))
                 continue
-            stack.append(int(tokens[i]))
+            stack.append(int(token))
         return stack[0]
-
