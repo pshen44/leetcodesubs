5dd6410e539c8ad8a5337347859d1ef2e58e1355
Patrick Shen <pzshen44@gmail.com>
2026-01-31 00:23:14 +0000

    [LeetCode Sync] Runtime - 23 ms (10.11%), Memory - 24.1 MB (5.03%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anamap = defaultdict(list) # [count] : str
-        for w in strs:
-            count = [0] * 26
-            for c in w:
-                count[ord('a') - ord(c)] += 1
-            anamap[tuple(count)].append(w)
-        return list(anamap.values())
+        hmap = defaultdict(list)
+        for s in strs:
+            scount = [0] * 26
+            for char in s:
+                scount[ord('a') - ord(char)] += 1
+            hmap[tuple(scount)].append(s)
+        return list(hmap.values())
