b541a6a04708741d790ac68945649757e9f1515a
Patrick Shen <pzshen44@gmail.com>
2026-01-31 02:01:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.9 MB (41.34%)

--- 0783-search-in-a-binary-search-tree/solution.py
+++ 0783-search-in-a-binary-search-tree/solution.py
@@ -6,6 +6,11 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        while root and root.val != val:
-            root = root.right if root.val < val else root.left
-        return root
+        while root:
+            if val < root.val:
+                root = root.left
+            elif val > root.val:
+                root = root.right
+            else:
+                return root
+        return None
