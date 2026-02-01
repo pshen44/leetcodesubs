3265c2c9a7c13857ba91f528fad64e4f6c71857a
Patrick Shen <pzshen44@gmail.com>
2026-02-01 21:50:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (28.19%)

--- 0094-binary-tree-inorder-traversal/solution.py
+++ 0094-binary-tree-inorder-traversal/solution.py
@@ -9,7 +9,7 @@ class Solution:
         res = []
         def inorder(root):
             if not root:
-                return root
+                return
             inorder(root.left)
             res.append(root.val)
             inorder(root.right)
