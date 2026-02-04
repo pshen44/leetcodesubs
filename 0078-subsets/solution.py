fdf942b539bb785d32a13dda3b34496e0624181f
Patrick Shen <pzshen44@gmail.com>
2026-02-04 22:36:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (28.79%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -6,13 +6,11 @@ class Solution:
             if i == len(nums):
                 res.append(subset.copy())
                 return
-            # pick nums[i]
             subset.append(nums[i])
             backtrack(i + 1)
-            
-            #don't pick
+
             subset.pop()
             backtrack(i + 1)
-
+        
         backtrack(0)
         return res
