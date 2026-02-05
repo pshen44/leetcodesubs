5b69c5bfbbdae0840077b48860826d80cf06ddfe
Patrick Shen <pzshen44@gmail.com>
2026-02-05 03:50:18 +0000

    [LeetCode Sync] Runtime - 26 ms (7.39%), Memory - 19.7 MB (34.66%)

--- 0039-combination-sum/solution.py
+++ 0039-combination-sum/solution.py
@@ -1,27 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res, subset = [], []
+
         def backtrack(i):
-            if sum(subset) == target and subset not in res:
+            if sum(subset) == target:
                 res.append(subset.copy())
                 return
-            if sum(subset) > target or i >= len(candidates): #or (sum(subset) + candidates[i]) > target:
+            if i == len(candidates) or sum(subset) > target:
                 return
-            
-            #pick same number
             subset.append(candidates[i])
             backtrack(i)
             subset.pop()
-
-            #pick candidates[i + 1]
-            subset.append(candidates[i])
             backtrack(i + 1)
-
-            #don't pick
-            subset.pop()
-            backtrack(i + 1)
-            
         backtrack(0)
         return res
-
-
