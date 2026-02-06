69f1451246525647d2ba3b96b927b007eef3e45b
Patrick Shen <pzshen44@gmail.com>
2026-02-06 22:23:26 +0000

    [LeetCode Sync] Runtime - 135 ms (93.42%), Memory - 41.6 MB (59.15%)

--- 0883-car-fleet/solution.py
+++ 0883-car-fleet/solution.py
@@ -3,9 +3,9 @@ class Solution:
         pairs = [(p, s) for p, s in zip(position, speed)]
         pairs.sort(reverse = True)
         stack = []
-        for pos, spd in pairs:
-            timeToTarg = (target - pos) / spd
-            stack.append(timeToTarg)
-            while len(stack) >= 2 and stack[-2] >= stack[-1]:
-                stack.pop()
+        for pair in pairs:
+            pos, speed = pair[0], pair[1]
+            time = (target - pos) / speed
+            if not stack or time > stack[-1]:
+                stack.append(time)
         return len(stack)
