dd62150b268ac14b0abcfbde7fef6ff8a8962446
Patrick Shen <pzshen44@gmail.com>
2026-02-06 05:20:06 +0000

    [LeetCode Sync] Runtime - 7 ms (22.42%), Memory - 22.4 MB (8.43%)

--- 0543-diameter-of-binary-tree/solution.py
+++ 0543-diameter-of-binary-tree/solution.py
@@ -7,12 +7,12 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        def inorder(node):
+        def dfs(node):
             if not node:
                 return 0
-            left = inorder(node.left)
-            right = inorder(node.right)
+            left = dfs(node.left)
+            right = dfs(node.right)
             self.res = max(self.res, left + right)
             return max(left, right) + 1
-        inorder(root)
+        dfs(root)
         return self.res
