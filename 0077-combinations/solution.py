b89cc9bcc63247d9c0ce591e8c2fede593d32306
Patrick Shen <pzshen44@gmail.com>
2026-02-06 23:46:28 +0000

    [LeetCode Sync] Runtime - 119 ms (28.64%), Memory - 61.4 MB (54.69%)

--- 0077-combinations/solution.py
+++ 0077-combinations/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res, subset = [], []
+        res = []
 
-        def backtrack(i):
-            if i > n:
-                if len(subset) == k:
-                    res.append(subset.copy())
+        def backtrack(i, subset, n, k):
+            if len(subset) == k:
+                res.append(subset.copy())
                 return
-            
+            if i > n:
+                return
             subset.append(i)
-            backtrack(i + 1)
-
+            backtrack(i + 1, subset, n, k)
             subset.pop()
-            backtrack(i + 1)
+            backtrack(i + 1, subset, n, k)
         
-        backtrack(1)
+        backtrack(1, [], n, k)
         return res
-        
