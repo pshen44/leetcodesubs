6f7eec94028b26f69b67016a657ea2df5f1d90e8
Patrick Shen <pzshen44@gmail.com>
2026-02-08 23:51:27 +0000

    [LeetCode Sync] Runtime - 178 ms (7.61%), Memory - 78.8 MB (10.16%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -2,53 +2,52 @@ class Node:
     def __init__(self, key, val):
         self.val = val
         self.key = key
-        self.prev = None
         self.next = None
+        self.prev = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {} # key : node(key, value)
+        self.cache = {} # key : Node(key, val)
 
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        
+        self.head = Node(-1, -1) #MOST REC USED
+        self.tail = Node(-1, -1) #LEAST REC USED
         self.head.next = self.tail
         self.tail.prev = self.head
-    def insert(self, node):
-        prvnode = self.tail.prev
-        nxtnode = self.tail
-        node.next = nxtnode
-        node.prev = prvnode
-        prvnode.next = node
-        nxtnode.prev = node
-
-    def remove(self, node):
-        prv = node.prev
-        nxt = node.next
-        prv.next = nxt
-        nxt.prev = prv
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            # {1 : 1}
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         else:
             return -1
-        
+
+    def insert(self, node):
+        NXT = self.head.next
+        self.head.next = node
+        node.next = NXT
+        NXT.prev = node
+        node.prev = self.head
+
+    def remove(self,node):
+        PRV = node.prev
+        NXT = node.next
+        NXT.prev = PRV
+        PRV.next = NXT
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
+
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
-        if len(self.cache) > self.capacity:
-            LRU = self.head.next
+        if self.capacity < len(self.cache):
+            LRU = self.tail.prev
+            delkey = LRU.key
+            del self.cache[delkey]
             self.remove(LRU)
-            del self.cache[LRU.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
