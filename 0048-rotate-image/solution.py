aa6aa47f4c02f8453ca21c4e5f3c9f92a46cbd11
Patrick Shen <pzshen44@gmail.com>
2026-02-10 18:55:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (29.31%)

--- 0048-rotate-image/solution.py
+++ 0048-rotate-image/solution.py
@@ -2,12 +2,20 @@ class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """ 
-        matrix.reverse()
-        DIM = len(matrix)
-        for r in range(DIM):
-            for c in range(1 + r, DIM):
-                temp = matrix[r][c]
-                matrix[r][c] = matrix[c][r]
-                matrix[c][r] = temp
+        """
+        L, R  = 0, len(matrix) - 1
+        while L < R:
+            for i in range(R - L):
+                T, B = L, R
+                temp = matrix[T][L + i]
+
+                matrix[T][L + i] = matrix[B - i][L]
+
+                matrix[B - i][L] = matrix[B][R - i]
+                
+                matrix[B][R - i] = matrix[T + i][R]
+
+                matrix[T + i][R] = temp
+            L += 1
+            R -= 1
 
