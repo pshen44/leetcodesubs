ea57c6b57221025c4e438172b8ff140a41cac353
Patrick Shen <pzshen44@gmail.com>
2026-02-15 01:43:41 +0000

    [LeetCode Sync] Runtime - 263 ms (23.80%), Memory - 21.6 MB (72.94%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,23 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[0 , 1] , [1 , 0] , [-1 , 0] , [0 , -1]]
         ROWS = len(grid)
         COLS = len(grid[0])
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
         count = 0
-        
-        def dfs(r, c):
+
+        def dfs(r, c) -> int:
             if (r < 0 or c < 0 or
-            r == ROWS or c == COLS or grid[r][c] == "0"):
-                return
-            
+                r >= ROWS or c >= COLS or
+                grid[r][c] == "0"):
+                return 0
+            # we are at valid square
             grid[r][c] = "0"
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
-        
+            return 1
+
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1":
-                    dfs(r, c)
-                    count += 1
+                count += dfs(r, c)
         return count
 
