e448cb6df7087d8f35843ce6a2d0dea2272a7437
Patrick Shen <pzshen44@gmail.com>
2026-02-18 23:03:29 +0000

    [LeetCode Sync] Runtime - 864 ms (22.49%), Memory - 68.2 MB (16.82%)

--- 0322-coin-change/solution.py
+++ 0322-coin-change/solution.py
@@ -1,11 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+        memo = {}
+        
+        def dfs(amount):
+            if amount == 0:
+                return 0
+            if amount in memo:
+                return memo[amount]
+            res = 1e9
+            for coin in coins:
+               if amount - coin >= 0: 
+                    res = min(res, 1 + dfs(amount - coin))
+            memo[amount] = res
+            return res
 
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        out = dfs(amount)
+        return -1 if out >= 1e9 else out
 
