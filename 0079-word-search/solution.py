499c6de6581a9ddda8338495a91192c26066b5a6
Patrick Shen <pzshen44@gmail.com>
2026-03-02 15:44:24 +0000

    [LeetCode Sync] Runtime - 4757 ms (19.29%), Memory - 19.4 MB (58.96%)

--- 0079-word-search/solution.py
+++ 0079-word-search/solution.py
@@ -2,28 +2,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS = len(board)
         COLS = len(board[0])
-        
-        path = set()
 
+        path = set()
+        
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            if (r < 0 or c < 0 or
-                r == ROWS or c == COLS or
-                board[r][c] != word[i] or
-                (r,c) in path):
+            if (min(r,c) < 0 or
+            r == ROWS or c == COLS or
+            (r,c) in path or board[r][c] != word[i]):
                 return False
             path.add((r,c))
+
             res = (dfs(r + 1, c, i + 1) or
-                    dfs(r - 1, c, i + 1) or
-                    dfs(r, c + 1, i + 1) or
-                    dfs(r, c - 1, i + 1))
+            dfs(r - 1, c, i + 1) or
+            dfs(r, c + 1, i + 1) or
+            dfs(r, c - 1, i + 1))
             path.remove((r,c))
+
             return res
 
+
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r, c, 0):
+                if dfs(r,c, 0):
                     return True
         return False
+
 
