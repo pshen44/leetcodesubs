c6bd2b9c29bbb6dc170d254d8a57313071c3aa80
Patrick Shen <pzshen44@gmail.com>
2026-06-24 05:30:26 +0000

    [LeetCode Sync] Runtime - 11 ms (77.48%), Memory - 19.5 MB (46.07%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,4 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
-        
+        smap, tmap = {}, {}
+        for c in s:
+            smap[c] = 1 + smap.get(c, 0)
+        for c in t:
+            tmap[c] = 1 + tmap.get(c, 0)
+        return smap == tmap
