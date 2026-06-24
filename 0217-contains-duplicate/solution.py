c206a239599d0bbd7eccdc1b12fee0f68197e3f7
Patrick Shen <pzshen44@gmail.com>
2026-06-24 05:28:16 +0000

    [LeetCode Sync] Runtime - 11 ms (82.26%), Memory - 31.2 MB (93.07%)

--- 0217-contains-duplicate/solution.py
+++ 0217-contains-duplicate/solution.py
@@ -1,10 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hmap = {}
-        for n in nums:
-            if n not in hmap:
-                hmap[n] = 1
-            else:
-                return True
-        return False
-        
+        return len(set(nums)) != len(nums)
